0c394dfd13b5e0e28d3fde85f09baa48c864ce1a
Brian Greig <brian@resurgencewebdesign.com>
2016-07-08 22:22:34 -0400

    Templates
    Added handlebars template.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 (function(){
-    var achievements = []
-    var theTemplateScript = $('#award-template').html();
-    var theTemplate = Handlebars.compile(theTemplateScript);
-    $.getJSON( "data.json", function ( data ){
-        var context = data;
-        var theCompiledHTML = theTemplate(context);
-        $('.content-placeholder').html(theCompiledHTML);
-   });   
+	var achievements = []
+	var theTemplateScript = $('#award-template').html();
+	var theTemplate = Handlebars.compile(theTemplateScript);
+	$.getJSON( "data.json", function ( data ){
+		var context = data;
+		var theCompiledHTML = theTemplate(context);
+		$('.content-placeholder').html(theCompiledHTML);
+	});   
 })();
 
